bd6ce68c74b9f86f91b71fa4668342ed6a871fcd
PeemaiT <plackychitehwe@gmail.com>
2023-02-24 10:05:48 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+
+ * print_numbers - checks for checks for a digit (0 through 9).
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+void print_numbers(1)
+
+{
+
+int c;
+
+
+for (c = 48; c < 58; c++);
+
+{
+
+_putchar(c);
+
+}
+
+_putchar('\n');
+
+ }
